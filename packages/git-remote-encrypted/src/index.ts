a80d2108230d7338e54a64222ef7d05d2b8c06ce
Callum Macdonald <git@callum-macdonald.com>
2020-10-08 22:34:27 +0300

    Walk trees and add all objects to the list.

--- packages/git-remote-encrypted/src/index.ts
+++ packages/git-remote-encrypted/src/index.ts
@@ -1,6 +1,6 @@
 import Bluebird from 'bluebird';
-import git from 'isomorphic-git';
 import fs from 'fs';
+import git, { TREE } from 'isomorphic-git';
 import http from 'isomorphic-git/http/node';
 import path from 'path';
 
@@ -15,7 +15,7 @@ const params = {
 export const start = async () => {
   if (DEBUG) console.log('dir #AGIM7a', params.dir);
 
-  const commits = new Set<string>();
+  const objectIds = new Set<string>();
   const trees = new Set<string>();
 
   const log = await git.log({ ...params });
@@ -23,23 +23,42 @@ export const start = async () => {
   if (DEBUG) console.log('Log #T71dMA', log);
 
   await Bluebird.each(log, logEntry => {
-    commits.add(logEntry.oid);
+    objectIds.add(logEntry.oid);
     trees.add(logEntry.commit.tree);
+  });
+
+  const walkerTrees = await Bluebird.map(trees, treeObjectId =>
+    TREE({ ref: treeObjectId })
+  );
+
+  await git.walk({
+    ...params,
+    trees: walkerTrees,
+    map: async (filename, entries) => {
+      if (entries === null) {
+        return filename;
+      }
+
+      await Bluebird.each(entries, async entry => {
+        objectIds.add(await entry.oid());
+      });
+
+      return filename;
+    },
   });
 
   if (DEBUG)
     console.log(
-      'Commits',
-      commits.entries(),
+      'Objects',
+      objectIds.entries(),
       'Trees',
       trees.entries(),
       '#Nzk9nr'
     );
 
-  await Bluebird.each(commits, async oid => {
-    const object = await git.readObject({ ...params, oid, format: 'wrapped' });
-    console.log('Found object #7bAlBa', object);
-  });
+  // await Bluebird.each(objectIds, async oid => {
+  //   const object = await git.readObject({ ...params, oid, format: 'wrapped' });
+  // });
 };
 
 start();
