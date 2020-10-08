f4216fa6c34bae3e03352f04dbd3e02e6b7c48b6
Callum Macdonald <git@callum-macdonald.com>
2020-10-08 19:57:29 +0300

    Get commit and tree hashes.

--- packages/git-remote-encrypted/src/index.ts
+++ packages/git-remote-encrypted/src/index.ts
@@ -1,3 +1,43 @@
+import Bluebird from 'bluebird';
+import git from 'isomorphic-git';
+import fs from 'fs';
+import http from 'isomorphic-git/http/node';
+import path from 'path';
+
+const DEBUG = true;
+
+const params = {
+  fs,
+  http,
+  dir: path.join(__dirname, '../', 'repo'),
+};
+
+export const start = async () => {
+  if (DEBUG) console.log('dir #AGIM7a', params.dir);
+
+  const commits = new Set<string>();
+  const trees = new Set<string>();
+
+  const log = await git.log({ ...params });
+
+  if (DEBUG) console.log('Log #T71dMA', log);
+
+  await Bluebird.each(log, logEntry => {
+    commits.add(logEntry.oid);
+    trees.add(logEntry.commit.tree);
+  });
+
+  console.log(
+    'Commits',
+    commits.entries(),
+    'Trees',
+    trees.entries(),
+    '#Nzk9nr'
+  );
+};
+
+start();
+
 export const sum = (a: number, b: number) => {
   if ('development' === process.env.NODE_ENV) {
     console.log('boop');
