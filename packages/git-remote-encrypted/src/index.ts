eaba2ab59e392dec6c9edf837594249d2ad1cece
Callum Macdonald <git@callum-macdonald.com>
2020-10-09 14:34:55 +0300

    Remove stub func.

--- packages/git-remote-encrypted/src/index.ts
+++ packages/git-remote-encrypted/src/index.ts
@@ -70,10 +70,3 @@ export const start = async () => {
 };
 
 start();
-
-export const sum = (a: number, b: number) => {
-  if ('development' === process.env.NODE_ENV) {
-    console.log('boop');
-  }
-  return a + b;
-};
