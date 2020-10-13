1d02974624feb4cd6c3052041a49920dbd0c7204
Callum Macdonald <git@callum-macdonald.com>
2020-10-13 15:46:51 +0300

    Use debug instead of file logging.

--- packages/git-remote-encrypted/src/cli.ts
+++ packages/git-remote-encrypted/src/cli.ts
@@ -13,37 +13,29 @@ const output = process.stdout;
 
 const dir = process.cwd();
 
-const devLog = async (message: string) => {
-  fs.promises.appendFile(
-    '/var/folders/3v/t9ntgj4n7175lh6fbrmspfh80000gn/T/cli.log',
-    `${message}\n`
-  );
-};
-
 input.on('data', async (line: string) => {
   log('Got line #VkenW7', line);
-  devLog(`Got line #H2oXh4 -- ${line}`);
 
   if (line === 'capabilities') {
     output.write('option\npush\nfetch\n\n');
-    devLog('Written capabilities #EGhHeC');
+    log('Written capabilities #EGhHeC');
   }
   if (line.startsWith('option')) {
     output.write('unsupported\n');
-    devLog('Written options unsupported #RURNt1');
+    log('Written options unsupported #RURNt1');
   }
   if (line.startsWith('list')) {
     //
   }
   if (line.startsWith('list for-push')) {
     output.write('? refs/heads/master\n@refs/heads/master HEAD\n\n');
-    devLog('Written list for-pusth #xgF2Xn');
+    log('Written list for-pusth #xgF2Xn');
   }
   if (line.startsWith('push')) {
     const [, refs] = line.split(' ');
     const [, dst] = refs.split(':');
     output.write(`ok ${dst}\n\n`);
-    devLog('Written push response #sfRvdP');
+    log('Written push response #sfRvdP');
   }
   if (line === '') {
     //
