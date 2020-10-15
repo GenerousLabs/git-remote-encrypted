e8f5007471d6cc493d98c4ad77ac647ff38e2fde
Callum Macdonald <git@callum-macdonald.com>
2020-10-15 17:58:46 +0300

    New crypto.

---
+++ packages/git-encrypted/src/crypto.test.ts
@@ -0,0 +1,79 @@
+import { decodeBase64, decodeUTF8 } from 'tweetnacl-util';
+import {
+  concat,
+  createNonce,
+  decryptFile,
+  encryptFile,
+  encryptFilename,
+  split,
+} from './crypto';
+import { KEYS } from './types';
+
+const keys: KEYS = {
+  content: decodeBase64('OTdY2G5jOtUb4NkTIrcMic5Om2FSGVNr+mOV21bMfkY='),
+  filename: decodeBase64('aeFYzTwOrPbAPu7Lyw1QZ34JglphbLTgAAHtjr2Zcps='),
+  salt: decodeBase64('wIfKspQFPMhcpxWSNO/d/aA50ErheC6t'),
+};
+
+const exampleFilename = '6fb50f57ebfe2687563a016a1cdce2f6ee445312';
+const exampleFilenameArray = decodeUTF8(exampleFilename);
+const exampleEncryptedFilename =
+  'ffdc5e936be06e3facc5ef99c0f387b9ba3a65b27c23328021bbbd60805b56db447f6aeb3a34b2b27b81b154169047969fc79a0ec2686801';
+const exampleString = 'This is a simple text string';
+const exampleArray = decodeUTF8(exampleString);
+
+describe('crypto', () => {
+  describe('cocnat()', () => {
+    it('Successfully combines arrays #UFUHj5', () => {
+      expect(
+        concat(Uint8Array.from([255, 1, 0, 255]), Uint8Array.from([13]))
+      ).toEqual(Uint8Array.from([255, 1, 0, 255, 13]));
+    });
+  });
+
+  describe('split()', () => {
+    it('Successfully splits an array #i2yH7q', () => {
+      expect(
+        split(
+          Uint8Array.from([255, 1, 0, 255, 13]),
+
+          4
+        )
+      ).toEqual([Uint8Array.from([255, 1, 0, 255]), Uint8Array.from([13])]);
+    });
+  });
+
+  describe('encryptFilename()', () => {
+    it('Encrypts to a known value #0IJkyE', async () => {
+      const nonce = await createNonce({
+        salt: keys.salt,
+        input: exampleFilenameArray,
+      });
+      const encryptedFilename = await encryptFilename({
+        filenameArray: exampleFilenameArray,
+        nonce,
+        keys,
+      });
+      expect(encryptedFilename).toEqual(exampleEncryptedFilename);
+    });
+  });
+
+  describe('encryptFile() and decryptFile()', () => {
+    it('Encrypts and decrypts #k279r6', async () => {
+      const [name, body] = await encryptFile({
+        filename: exampleFilename,
+        contents: exampleArray,
+        keys,
+      });
+      const [originalName, originalBody] = await decryptFile({
+        fileContents: body,
+        encryptedFilenameHex: name,
+        keys,
+      });
+
+      expect(originalName).toEqual(exampleFilename);
+      expect(originalBody).toEqual(exampleArray);
+      expect(name).toEqual(exampleEncryptedFilename);
+    });
+  });
+});
---
+++ packages/git-encrypted/src/crypto.ts
@@ -0,0 +1,147 @@
+import { arrayToHex, hexToArray } from 'enc-utils';
+import { hash, randomBytes, secretbox } from 'tweetnacl';
+import { decodeUTF8, encodeBase64, encodeUTF8 } from 'tweetnacl-util';
+import { NONCE_LENGTH } from './constants';
+import { KEYS } from './types';
+
+export const concat = (a: Uint8Array, b: Uint8Array) => {
+  const length = a.length + b.length;
+  const result = new Uint8Array(length);
+  result.set(a, 0);
+  result.set(b, a.length);
+  return result;
+};
+
+export const split = (combined: Uint8Array, length: number) => {
+  const a = combined.slice(0, length);
+  const b = combined.slice(length);
+  return [a, b];
+};
+
+export const generateKey = (length: number) =>
+  encodeBase64(randomBytes(length));
+
+/**
+ * Given an input and a salt, combine them into an nonce of the correct length.
+ * This is a deterministic operation.
+ *
+ * NOTE: Any feedback from cryptographers welcome here. This might be a
+ * terrible idea.
+ */
+export const createNonce = async ({
+  salt,
+  input,
+}: {
+  salt: Uint8Array;
+  input: Uint8Array;
+}) => {
+  const combined = concat(input, salt);
+  const hashed = hash(combined);
+  const [nonce] = split(hashed, NONCE_LENGTH);
+  return nonce;
+};
+
+// Copy the same API as secretbox
+export const encrypt = async (
+  msg: Uint8Array,
+  nonce: Uint8Array,
+  key: Uint8Array
+) => {
+  const result = secretbox(msg, nonce, key);
+  return result;
+};
+
+// Copy the same API as secretbox.open
+export const decrypt = async (
+  box: Uint8Array,
+  nonce: Uint8Array,
+  key: Uint8Array
+) => {
+  const decrypted = secretbox.open(box, nonce, key);
+  if (decrypted === null) {
+    throw new Error('Decryption failed. #FxljFi');
+  }
+  return decrypted;
+};
+
+/**
+ * Given a cleartext filename, calculate the encrypted filename
+ *
+ * NOTE: This cannot be decrypted. The nonce will be lost. `encryptFile()`
+ * saves the nonce into the file body.
+ */
+export const encryptFilename = async ({
+  filenameArray,
+  keys,
+  nonce,
+}: {
+  filenameArray: Uint8Array;
+  nonce: Uint8Array;
+  keys: KEYS;
+}) => {
+  const encryptedFilenameArray = await encrypt(
+    filenameArray,
+    nonce,
+    keys.filename
+  );
+  const encryptedFilenameHex = arrayToHex(encryptedFilenameArray);
+  return encryptedFilenameHex;
+};
+
+/**
+ * Given a cleartext filename and a contents, create an nonce then use that to
+ * encrypt the filename, encrypt the file contents, and combine the nonce and
+ * encrypted file contents, returning both the encrypted filename (hex encoded)
+ * and the encrypted body combined with the nonce.
+ */
+export const encryptFile = async ({
+  filename,
+  contents,
+  keys,
+}: {
+  filename: string;
+  contents: Uint8Array;
+  keys: KEYS;
+}): Promise<[string, Uint8Array]> => {
+  const filenameArray = decodeUTF8(filename);
+  const nonce = await createNonce({ salt: keys.salt, input: filenameArray });
+
+  const encryptedFilenameHex = await encryptFilename({
+    filenameArray,
+    keys,
+    nonce,
+  });
+
+  const encryptedContents = await encrypt(contents, nonce, keys.content);
+  const noncePlusEncryptedContent = concat(nonce, encryptedContents);
+
+  return [encryptedFilenameHex, noncePlusEncryptedContent];
+};
+
+export const decryptFile = async ({
+  fileContents,
+  encryptedFilenameHex,
+  keys,
+}: {
+  encryptedFilenameHex: string;
+  fileContents: Uint8Array;
+  keys: KEYS;
+}): Promise<[string, Uint8Array]> => {
+  const [nonce, encryptedContents] = split(fileContents, NONCE_LENGTH);
+
+  const encryptedFilenameArray = hexToArray(encryptedFilenameHex);
+  const decryptedFilenameArray = await decrypt(
+    encryptedFilenameArray,
+    nonce,
+    keys.filename
+  );
+  const decryptedFilename = encodeUTF8(decryptedFilenameArray);
+
+  const decryptedContents = await decrypt(
+    encryptedContents,
+    nonce,
+    keys.content
+  );
+
+  return [decryptedFilename, decryptedContents];
+};
