268b7908ef37ae5676e08b7dd572066ef1696c8c
Callum Macdonald <git@callum-macdonald.com>
2020-10-16 22:27:43 +0300

    Bugfix. Keys are base64 encoded, not utf8.

--- packages/git-encrypted/src/keys.ts
+++ packages/git-encrypted/src/keys.ts
@@ -1,5 +1,5 @@
 import { mapValues } from 'remeda';
-import { decodeUTF8, encodeUTF8 } from 'tweetnacl-util';
+import { decodeBase64, encodeBase64 } from 'tweetnacl-util';
 import { KEYS, KEYS_STRINGS } from './types';
 
 export const stringToKeys = ({
@@ -7,11 +7,11 @@ export const stringToKeys = ({
 }: {
   keysString: KEYS_STRINGS;
 }): KEYS => {
-  const keys = mapValues(keysString, decodeUTF8);
+  const keys = mapValues(keysString, decodeBase64);
   return keys;
 };
 
 export const keysToString = ({ keys }: { keys: KEYS }): KEYS_STRINGS => {
-  const keysString = mapValues(keys, encodeUTF8);
+  const keysString = mapValues(keys, encodeBase64);
   return keysString;
 };
