079a47c373869edeb26202fde29f445e452c0972
Callum Macdonald <git@callum-macdonald.com>
2020-10-16 22:57:02 +0300

    Warn and exit on key load failure.

--- packages/git-encrypted/src/api/getKeysFromDisk.ts
+++ packages/git-encrypted/src/api/getKeysFromDisk.ts
@@ -6,9 +6,14 @@ export const getKeysFromDisk = async ({
   fs,
   gitdir,
 }: Pick<GitBaseParams, 'fs' | 'gitdir'>) => {
-  const keysPath = getKeysPath({ gitdir });
-  const keysJson = await fs.promises.readFile(keysPath, 'utf8');
-  const keysString: KEYS_STRINGS = JSON.parse(keysJson);
-  const keys = stringToKeys({ keysString });
-  return keys;
+  try {
+    const keysPath = getKeysPath({ gitdir });
+    const keysJson = await fs.promises.readFile(keysPath, 'utf8');
+    const keysString: KEYS_STRINGS = JSON.parse(keysJson);
+    const keys = stringToKeys({ keysString });
+    return keys;
+  } catch (error) {
+    console.error('FATAL: Failed to load keys. #TGIwoU');
+    process.exit();
+  }
 };
