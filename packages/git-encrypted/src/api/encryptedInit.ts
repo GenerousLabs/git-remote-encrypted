97e1982972d3b9c5a010429c0af414ccd375614b
Callum Macdonald <git@callum-macdonald.com>
2020-10-16 22:29:02 +0300

    First attempt at key generation.
    This is a bit meaningless without actually loading the keys from disk, but it seems to work.

--- packages/git-encrypted/src/api/encryptedInit.ts
+++ packages/git-encrypted/src/api/encryptedInit.ts
@@ -1,3 +1,4 @@
+import { isEmptyRepo } from '../git';
 import { packageLog } from '../log';
 import {
   EncryptedRemoteParams,
@@ -13,6 +14,8 @@ import {
   getEncryptedObjectsDir,
   getEncryptedRefsDir,
 } from '../utils';
+import { createKeys } from './createKeys';
+import { saveKeysToDisk } from './saveKeysToDisk';
 
 const log = packageLog.extend('encryptedInit');
 
@@ -42,11 +45,21 @@ export const encryptedInit = async (
   log('Invoked #rv3T39', JSON.stringify({ gitdir }));
 
   const encryptedDir = getEncryptedDir({ gitdir });
+  const encryptedKeysDir = getEncryptedKeysDir({ gitdir });
+
+  // Check if the directories exist BEFORE creating them
+  const encryptedDirectoryExists = await doesDirectoryExist({
+    fs,
+    path: encryptedDir,
+  });
+  const encryptedKeysDirectoryExists = await doesDirectoryExist({
+    fs,
+    path: encryptedKeysDir,
+  });
+
   await ensureDirectoryExists({ fs, path: encryptedDir });
 
   const encryptedGitDir = getEncryptedGitDir({ gitdir });
-
-  // TODO Add key init here
 
   // If the `encrypted/.git` directory exists, then we assume the repository has
   // already been initialised.
@@ -57,6 +70,19 @@ export const encryptedInit = async (
   }
 
   await gitApi.clone({ ...base, encryptedDir });
+
+  // If the directory did not exist, and we cloned an empty repo, then let's
+  // create and save new keys now.
+  if (!encryptedDirectoryExists && !encryptedKeysDirectoryExists) {
+    if (await isEmptyRepo({ fs, gitdir: encryptedGitDir })) {
+      const keys = await createKeys();
+      await saveKeysToDisk({ fs, gitdir, keys });
+      log('Creating a new set of keys #1QkKPD');
+      console.error(
+        'New keys created and saved to .git/encrypted-keys/keys.json. Back them up! #35GYES'
+      );
+    }
+  }
 
   // NOTE: We need to run this AFTER the clone because otherwise git complains
   // of trying to clone into a not empty directory.
