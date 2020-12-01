cc5c3328aa3ef38bb083aa8995ba9af3bdeec8c6
Callum Macdonald <git@callum-macdonald.com>
2020-12-01 16:17:47 +0100

    Export `createKeys()`.

--- packages/git-encrypted/src/index.ts
+++ packages/git-encrypted/src/index.ts
@@ -2,6 +2,7 @@ export { encryptedInit } from './api/encryptedInit';
 export { encryptedPush, EncryptedPushResult } from './api/encryptedPush';
 export { encryptedFetch } from './api/encryptedFetch';
 export { getEncryptedRefObjectId } from './api/getEncryptedRefObjectId';
+export { createKeys } from './api/createKeys';
 export { getKeysFromDisk } from './api/getKeysFromDisk';
 export { saveKeysToDisk } from './api/saveKeysToDisk';
 export { getRefsGitString } from './api/getRefsGitString';
