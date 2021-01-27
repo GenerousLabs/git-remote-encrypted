a082a63f9c586331fb4865c7e59ebf734b06ec2e
Johannes Bittner <johannes.bittner@gmail.com>
2021-01-27 12:39:04 +0100

    Rename 'hash' to 'deriveKey' and implement 'deriveKeys'
    deriveKeys returns a 'Keys' object

--- packages/git-encrypted/src/keyDerivation.test.ts
+++ packages/git-encrypted/src/keyDerivation.test.ts
@@ -1,29 +1,72 @@
-import { hash } from './keyDerivation';
+import { deriveKey, deriveKeys } from './keyDerivation';
+import { Keys } from './types';
 
 describe('keyDerivation', () => {
-  describe('hash()', () => {
+  describe('deriveKey()', () => {
     it('Creates a hash from the password "foobar"', () => {
       return expect(
-        hash({
+        deriveKey({
           password: 'foobar',
           salt: 'foosalt',
           cpuCost: 1024,
           blockSize: 8,
           parallelizationCost: 1,
+          keyLength: 96
         })
       ).resolves.toMatchSnapshot();
     });
 
     it('Returns a hash with a length of 96 bytes', () => {
       return expect(
-        hash({
+        deriveKey({
           password: 'quxbaz',
           salt: 'deadbeef',
           cpuCost: 1024,
           blockSize: 8,
           parallelizationCost: 1,
+          keyLength: 96,
         })
       ).resolves.toHaveLength(96);
+    });
+  });
+
+  describe('deriveKeys()', () => {
+    it('Returns the same value as deriveKey(), split in three parts', () => {
+      const password = 'foobar';
+      const salt = 'foosalt';
+      const cpuCost = 1024;
+      const blockSize = 8;
+      const parallelizationCost = 1;
+      const keyLength = 96;
+
+      const key1Promise: Promise<Uint8Array> = deriveKey({
+        password,
+        salt,
+        cpuCost,
+        blockSize,
+        parallelizationCost,
+        keyLength
+      });
+      const key2Promise: Promise<Keys> = deriveKeys({
+        password,
+        salt,
+        cpuCost,
+        blockSize,
+        parallelizationCost
+      });
+
+      key2Promise.then(key2 => {
+        const mergedArray = new Uint8Array(
+          key2.content.length +
+          key2.filename.length +
+          key2.salt.length);
+
+        mergedArray.set(key2.content);
+        mergedArray.set(key2.filename, key2.content.length);
+        mergedArray.set(key2.salt, key2.content.length + key2.filename.length);
+
+        expect(key1Promise).resolves.toEqual(mergedArray);
+      });
     });
   });
 });
--- packages/git-encrypted/src/keyDerivation.ts
+++ packages/git-encrypted/src/keyDerivation.ts
@@ -1,23 +1,25 @@
 import { scrypt } from 'scrypt-js';
+import { Keys } from './types';
 
-export const hash = async ({
+export const deriveKey = async ({
   password,
   salt,
   cpuCost,
   blockSize,
   parallelizationCost,
+  keyLength
 }: {
   password: string;
   salt: string;
   cpuCost: number;
   blockSize: number;
   parallelizationCost: number;
-}) => {
+  keyLength: number;
+}): Promise<Uint8Array> => {
   const passwordBuffer = Buffer.from(password.normalize('NFKC'));
   const saltBuffer = Buffer.from(salt.normalize('NFKC'));
-  const keyLength = 96;
 
-  const hash = await scrypt(
+  return await scrypt(
     passwordBuffer,
     saltBuffer,
     cpuCost,
@@ -25,6 +27,34 @@ export const hash = async ({
     parallelizationCost,
     keyLength
   );
+};
 
-  return hash;
+export const deriveKeys = async ({
+  password,
+  salt,
+  cpuCost,
+  blockSize,
+  parallelizationCost
+}: {
+  password: string;
+  salt: string;
+  cpuCost: number;
+  blockSize: number;
+  parallelizationCost: number;
+}): Promise<Keys> => {
+  const keyLength = 96;
+  const derivedKey = await deriveKey({
+    password,
+    salt,
+    cpuCost,
+    blockSize,
+    parallelizationCost,
+    keyLength
+  });
+
+  return {
+    content: derivedKey.slice(0, 32),
+    filename: derivedKey.slice(32, 64),
+    salt: derivedKey.slice(64, 96)
+  };
 };
